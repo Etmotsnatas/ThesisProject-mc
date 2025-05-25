7829def91dbac717da4a61fc55e5b57a0228cf3a
Etmotsnatas <myrsol@gmail.com>
2025-05-25 21:48:19 +0200

    Finished

--- ThesisProject/Assets/TutorialProject/Scripts/CameraLook.cs
+++ ThesisProject/Assets/TutorialProject/Scripts/CameraLook.cs
@@ -6,8 +6,11 @@ public class CameraLook : MonoBehaviour
 {
     [SerializeField] int mouseSens;
     [SerializeField] Camera playerCamera;
+    [SerializeField] Transform playerBody;
+    [SerializeField] Vector3 thirdPersonOffset = new Vector3(0, 2, -3);
     float xRotation, yRotation;
     float mouseX, mouseY;
+    bool isFirstPerson = true;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -20,17 +23,26 @@ public class CameraLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        
+             
         xRotation -= mouseY * Time.deltaTime * mouseSens;
         yRotation += mouseX * Time.deltaTime * mouseSens;
-
         xRotation = Mathf.Clamp(xRotation, -90, 90);
-
         transform.rotation = Quaternion.Euler(0,yRotation,0);
         playerCamera.transform.localRotation = Quaternion.Euler(xRotation,0,0);
 
-       
+        if (isFirstPerson)
+        {
+            playerCamera.transform.localPosition = Vector3.zero;
+            playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        }
+        else
+        {
+            Vector3 targetPosition = playerBody.position + Quaternion.Euler(0, playerBody.eulerAngles.y, 0) * thirdPersonOffset;
+            playerCamera.transform.position = targetPosition;
+            playerCamera.transform.LookAt(playerBody.position + Vector3.up * 1.5f);
+        }
+
+
     }
 
     private void OnLook(InputValue input)
@@ -38,5 +50,9 @@ public class CameraLook : MonoBehaviour
         mouseX = input.Get<Vector2>().x;
         mouseY = input.Get<Vector2>().y;
 
+    }
+    private void OnToggleView(InputValue input)
+    {
+        isFirstPerson = !isFirstPerson;
     }
 }
